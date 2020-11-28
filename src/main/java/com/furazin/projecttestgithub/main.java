b7f12646f3ae0eb7fa212fa1e17d06dc9bfdbbfa
dev_dastle <luis_castle_2011@hotmai.com>
2020-11-28 01:15:00 -0500

    Agregando workflow archivo al repositorio

--- src/main/java/com/furazin/projecttestgithub/main.java
+++ src/main/java/com/furazin/projecttestgithub/main.java
@@ -4,6 +4,7 @@ import com.furazin.projecttestgithub.Math.Arithmetic;
 
 public class main {
     public static void main(String[] args) {
+        /* Comentario */
         Arithmetic arith = new Arithmetic(3,3);
         System.out.println(arith.add());
     }
